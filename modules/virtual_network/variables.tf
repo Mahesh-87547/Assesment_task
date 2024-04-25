41755dedb359ca967e7c98942b7ea969e3d314e5
Mahesh-87547 <146524564+Mahesh-87547@users.noreply.github.com>
2024-04-25 12:40:25 +0530

    Create variables.tf

---
+++ modules/virtual_network/variables.tf
@@ -0,0 +1,15 @@
+ariable "virtual_network_name" {
+  description = "Name of the virtual network"
+}
+
+variable "address_space" {
+  description = "Address space of the virtual network"
+}
+
+variable "subnet_name" {
+  description = "Name of the subnet"
+}
+
+variable "subnet_address_prefixes" {
+  description = "Address prefix for the subnet"
+}
